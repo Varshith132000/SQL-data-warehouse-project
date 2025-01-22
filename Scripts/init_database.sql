1bc15886841462b5cd3d913fff8c63fb8a45ca24
Varshith132000 <varshith.work13@gmail.com>
2025-01-22 12:47:51 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,18 @@
+Drop and recent the Datawarehouse database
+if exists (select 1 from sys.database where name = 'datawarehouse'
+
+  ----Create Database 'DataWarehouse'
+use master;
+
+create database Datawarehouse;
+
+use Datawarehouse;
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+
+
 
